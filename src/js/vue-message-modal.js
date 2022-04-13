e5238694be8db249536f2aa063127967aefe8da4
tc-kyano <k.yano@timeconcier.jp>
2022-04-13 14:07:43 +0900

    ラジオボタンを作成

--- src/js/vue-message-modal.js
+++ src/js/vue-message-modal.js
@@ -2,6 +2,7 @@ Vue.component('vue-modal', {
   model: {
     prop: 'dialog',
     event: 'change',
+    radio: '未選択'
   },
   props: ['dialog'],
   computed: {
@@ -27,6 +28,15 @@ Vue.component('vue-modal', {
     :show-close="false"
     :before-close="handleClose"
   >
+    // タイミング指定
+    <div>
+      <span>配信区分</span>
+      <el-radio v-model="radio" label="未選択">未選択</el-radio>
+      <el-radio v-model="radio" label="確認後配信">確認後配信</el-radio>
+      <el-radio v-model="radio" label="予約配信(一回のみ)">予約配信(一回のみ)</el-radio>
+      <el-radio v-model="radio" label="予約配信(繰り返し)">予約配信(繰り返し)</el-radio>
+      <el-radio v-model="radio" label="今すぐ配信">今すぐ配信</el-radio>
+    </div>
     <span>This is a message</span>
     <span slot="footer" class="dialog-footer">
       <el-button @click="$emit('change', false)">Cancel</el-button>
